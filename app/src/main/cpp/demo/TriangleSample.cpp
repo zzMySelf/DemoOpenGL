2c1963d82687bb8adf72ecb47ab7349930dab01b
zhangyelei <zhangyelei@baidu.com>
2024-01-29 16:17:00 +0800

    增加glm库

---
+++ app/src/main/cpp/demo/TriangleSample.cpp
@@ -0,0 +1,13 @@
+//
+// Created by zhangyelei on 2024/1/26.
+//
+
+#include "TriangleSample.h"
+
+TriangleSample::TriangleSample() {
+
+}
+
+TriangleSample::~TriangleSample() {
+
+}
---
+++ app/src/main/cpp/demo/TriangleSample.h
@@ -0,0 +1,17 @@
+//
+// Created by zhangyelei on 2024/1/26.
+//
+
+#ifndef DEMOOPENGL_TRIANGLESAMPLE_H
+#define DEMOOPENGL_TRIANGLESAMPLE_H
+
+
+class TriangleSample {
+public:
+    TriangleSample();
+
+    ~TriangleSample();
+};
+
+
+#endif //DEMOOPENGL_TRIANGLESAMPLE_H
