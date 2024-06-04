5e6576f3577ca06922e15f5ec8cfd488bf03b56c
zhangyelei <zhangyelei@baidu.com>
2024-06-04 15:45:08 +0800

    一个vao 对应一个vbo 绘制两个三角形，通过index解决

--- app/src/main/cpp/demo/sample/TextureMapSample.cpp
+++ app/src/main/cpp/demo/sample/TextureMapSample.cpp
@@ -23,16 +23,21 @@ void TextureMapSample::loadImage(NativeImage *pImage) {
 }
 
 void TextureMapSample::init() {
+    // 生成纹理对象  生成纹理对象的数量，  纹理指针
     glGenTextures(1, &m_TextureId);
+    // 绑定纹理
     glBindTexture(GL_TEXTURE_2D, m_TextureId);
 
+    // 横轴\数轴拉伸方式，GL_CLAMP_TO_EDGE： 超出纹理范围的部分会被截取边缘值
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // 缩小过滤方式为GL_LINEAR 双线性插值来获取纹理像素颜色
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     glBindTexture(GL_TEXTURE_2D, GL_NONE);
 
+    // 顶点着色器
     char vShaderStr[] =
             "#version 300 es                            \n"
             "layout(location = 0) in vec4 a_position;   \n"
@@ -44,6 +49,7 @@ void TextureMapSample::init() {
             "   v_texCoord = a_texCoord;                \n"
             "}                                          \n";
 
+    // 片元着色器 用于对顶点着色器传递过来的数据进行处理
     char fShaderStr[] =
             "#version 300 es                                     \n"
             "precision mediump float;                            \n"
@@ -56,8 +62,10 @@ void TextureMapSample::init() {
             "  //outColor = texelFetch(s_TextureMap,  ivec2(int(v_texCoord.x * 404.0), int(v_texCoord.y * 336.0)), 0);\n"
             "}                                                   \n";
 
+    // 创建着色器程序
     m_ProgramObj = GLUtils::CreateProgram(vShaderStr, fShaderStr, m_VertexShader, m_FragmentShader);
     if (m_ProgramObj) {
+        // 返回指定统一变量的位置索引
         m_SamplerLoc = glGetUniformLocation(m_ProgramObj, "s_TextureMap");
     } else {
         Logger::d("TextureMapSample","TextureMapSample::Init create program fail");
@@ -70,6 +78,7 @@ void TextureMapSample::draw(int screenW, int screenH) {
     glClear(GL_STENCIL_BUFFER_BIT | GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(1.0, 1.0, 1.0, 1.0);
 
+    // 顶点坐标数组 顺时针 左上  左下 右下 右上
     GLfloat verticesCoords[] = {
             -1.0f,  0.5f, 0.0f,  // Position 0
             -1.0f, -0.5f, 0.0f,  // Position 1
@@ -77,6 +86,7 @@ void TextureMapSample::draw(int screenW, int screenH) {
             1.0f,  0.5f, 0.0f,   // Position 3
     };
 
+    // 纹理坐标数组
     GLfloat textureCoords[] = {
             0.0f,  0.0f,        // TexCoord 0
             0.0f,  1.0f,        // TexCoord 1
@@ -89,18 +99,34 @@ void TextureMapSample::draw(int screenW, int screenH) {
     //upload RGBA image data
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, m_TextureId);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_RenderImage.width, m_RenderImage.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_RenderImage.ppPlane[0]);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 GL_RGBA,
+                 m_RenderImage.width,
+                 m_RenderImage.height,
+                 0,
+                 GL_RGBA,
+                 GL_UNSIGNED_BYTE,
+                 m_RenderImage.ppPlane[0]);
     glBindTexture(GL_TEXTURE_2D, GL_NONE);
 
     // Use the program object
     glUseProgram (m_ProgramObj);
 
     // Load the vertex position
-    glVertexAttribPointer (0, 3, GL_FLOAT,
-                           GL_FALSE, 3 * sizeof (GLfloat), verticesCoords);
+    glVertexAttribPointer (0,
+                           3,
+                           GL_FLOAT,
+                           GL_FALSE,
+                           3 * sizeof (GLfloat),
+                           verticesCoords);
     // Load the texture coordinate
-    glVertexAttribPointer (1, 2, GL_FLOAT,
-                           GL_FALSE, 2 * sizeof (GLfloat), textureCoords);
+    glVertexAttribPointer (1,
+                           2,
+                           GL_FLOAT,
+                           GL_FALSE,
+                           2 * sizeof (GLfloat),
+                           textureCoords);
 
     glEnableVertexAttribArray (0);
     glEnableVertexAttribArray (1);
